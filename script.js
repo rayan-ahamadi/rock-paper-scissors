97ec34eb98a2e73c8a03282c122ea799102f4f16
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2024-01-09 15:56:42 +0100

    implémentation de la nouvelle logique JS  pour l'interface graphique
    modification du code javascript avec manipulation du DOM pour
    l'adapter à l'interface graphique HTML

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let pierre = document.querySelector('#pierre');
-let feuille = document.querySelector('#feuille');
-let ciseaux = document.querySelector('#ciseaux');
+const pierre = document.querySelector('#pierre');
+const feuille = document.querySelector('#feuille');
+const ciseaux = document.querySelector('#ciseaux');
 
+const choicePlayerImg = document.querySelector(".choice-player img")
+const choiceComputerImg = document.querySelector(".choice-ia img")
+const choicePlayerTxt = document.querySelector(".choice-player p")
+const choiceComputerTxt = document.querySelector(".choice-ia p")
+const choiceResult = document.querySelector(".choice-result p")
+
+const scorePlayerTxt = document.querySelector('#player-score');
+const scoreComputerTxt = document.querySelector('#ia-score');
+
+const resultGame = document.querySelector('.result-game p');
+const divResultGame = document.querySelector('.result-game');
+const restartButton = document.createElement('button');
+restartButton.textContent = 'Rejouer ?';
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
    let choice = ['pierre', 'feuille', 'ciseaux'];
@@ -9,12 +25,31 @@ function getComputerChoice() {
    return choice[random];
 }
 
+function restartGame(){
+    playerScore = 0;
+    computerScore = 0;
+    scorePlayerTxt.textContent = playerScore;
+    scoreComputerTxt.textContent = computerScore;
+    choicePlayerImg.src = `img/thinking.jpeg`;
+    choicePlayerTxt.textContent = `Choisissez un élément pour commencer...`;
+    choiceComputerImg.src = `./img/thinking.jpeg`;
+    choiceComputerTxt.textContent = `L'ordinateur choisit un élément...`;
+    choiceResult.textContent = '';
+    resultGame.textContent = '';
+    divResultGame.removeChild(restartButton);
+}
 
-pierre.addEventListener('click', function("pierre"));
-feuille.addEventListener('click', function("feuille"));
-ciseaux.addEventListener('click', function("ciseaux"));
+pierre.addEventListener('click', () => {
+    gameRound("pierre");
+});
+feuille.addEventListener('click', ()=>{
+    gameRound("feuille");
+});
+ciseaux.addEventListener('click', ()=>{
+    gameRound("ciseaux");
+});
 
-function gameRound(playerChoice, computerChoice) {
+function roundWinner(playerChoice, computerChoice) {
     switch (playerChoice) {
         case 'pierre':
             if (computerChoice === 'pierre') {
@@ -45,46 +80,44 @@ function gameRound(playerChoice, computerChoice) {
     }
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while((playerScore < 5) && (computerScore < 5)){
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let result = gameRound(playerChoice, computerChoice);
-        console.log('Vous avez choisi ' + playerChoice + ' et l\'ordinateur a choisi ' + computerChoice);
-        console.log(result);
+function gameRound(playerChoice){
+    let computerChoice = getComputerChoice();
+    if((playerScore < 5) && (computerScore < 5)){
+        let result = roundWinner(playerChoice, computerChoice);
+        choicePlayerImg.src = `./img/${playerChoice}.png`;
+        choicePlayerTxt.textContent = `Vous avez choisi : ${playerChoice}`;
+        choiceComputerImg.src = `./img/${computerChoice}.png`;
+        choiceComputerTxt.textContent = `L'ordinateur a choisi : ${computerChoice}`;
+        choiceResult.textContent = result;
         if(result.includes('Gagné')){
             playerScore++;
         } else if(result.includes('Perdu')){
             computerScore++;
-        }else {
-            round--;
         }
-        console.log('Vous avez ' + playerScore + ' point(s).' + 'Et l\'ordinateur a ' + computerScore + ' point(s)');
+
+        scorePlayerTxt.textContent = playerScore;
+        scoreComputerTxt.textContent = computerScore;
+
+        
     }
 
-    if(playerScore > computerScore){
-        console.log('Vous avez gagné la partie');
-    } else if(playerScore < computerScore){
-        console.log('Vous avez perdu la partie');
-    } else {
-        console.log('Egalité');
+    if(playerScore > computerScore && playerScore === 5){
+        resultGame.textContent = 'Vous avez gagné la partie !';
+        divResultGame.appendChild(restartButton);
+        restartButton.addEventListener('click', restartGame);
+    } else if(playerScore < computerScore && computerScore === 5){
+        resultGame.textContent = 'Vous avez perdu la partie !';
+        divResultGame.appendChild(restartButton);
+        restartButton.addEventListener('click', restartGame);
     }
 
-    let rematch = prompt('Voulez-vous rejouer ?, (oui ou non)');
-    rematch = rematch.toLowerCase().replace(/\s/g, '');
-    while (rematch !== 'oui' && rematch !== 'non') {
-        //rematch = prompt('Erreur : Voulez-vous rejouer ?, (oui ou non)');
-        rematch = rematch.toLowerCase().replace(/\s/g, '');
+    
+
+
     }
 
-    if (rematch === 'oui') {
-        game();
-    }
 
-}
+  
 
-game()
+
 
